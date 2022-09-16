52566b618b0619d3cf9582e5712590c03a23c9bc
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-09-16 13:39:03 -0500

    Complete oop.rb and self-eval

--- oop.rb
+++ oop.rb
@@ -1,30 +1,127 @@
 # Write a class called Unicorn
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
-# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+# it should have a method called "say" that returns whatever string is passed in,
+# with "*~*" at the beginning and end of the string
+class Unicorn
+  def initialize(name)
+    @name = name
+    @color = "silver"
+  end
+  def say(string)
+    p "*~* #{string} *~*"
+end
+end
+unicorn1 = Unicorn.new("Jackson")
+p unicorn1
+unicorn1.say("Habalabadingdong")
+
 
 
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
-#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
+#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info
+#  is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
-
-
+class Vampire
+  def initialize(name)
+    @name = name
+    @pet = "bat"
+    @thirsty = true
+  end
+  def change_pet(new_pet)
+    @pet = new_pet
+  end
+  def drink
+    @thirsty = false
+  end
+end
+vampire1 = Vampire.new("Serena")
+p vampire1
+vampire1.drink
+p vampire1
+vampire1.change_pet("cat")
+p vampire1
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
 #  it should have a dynamic rider attribute (string)
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+  def initialize(name,rider,color)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = true
+    @dragoneat = 0
+   end
+   def eat
+     if @dragoneat >0
+     puts "#{@name} ate #{@dragoneat+1} knights!"
+   else
+     puts "#{@name} ate #{@dragoneat+1} knight!"
+   end
+     @dragoneat += 1
+     if
+       @dragoneat == 4
+       @is_hungry = false
+     end
+   end
+ end
 
 
+
+
+dragon1 = Dragon.new("Toothless","Hiccup","Black")
+p dragon1
+dragon1.eat
+dragon1.eat
+dragon1.eat
+dragon1.eat
+
+p dragon1
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
 #  it should have a dynamic disposition attribute (string)
 #  it should have an age attribute that defaults to 0
 #  it should have a celebrate_birthday method. When called, the age increases by 1
-#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
+#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33,
+#  it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+  def initialize(name,disposition)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @is_adult =false
+    @is_old = false
+    @has_ring = false
+    if @name =="Frodo"
+      @has_ring=true
+    end
+  end
+  def celebrate_birthday
+    @age += 1
+    if @age>=33
+      @is_adult =true
+    else @age>=101
+      @is_old =true
+    end
+  end
+end
+
+hobbit1 = Hobbit.new("Frodo","Moody AF")
+p hobbit1
+hobbit1.celebrate_birthday
+p hobbit1
+32.times {hobbit1.celebrate_birthday}
+p hobbit1
+68.times {hobbit1.celebrate_birthday}
+p hobbit1
+
+hobbit2 = Hobbit.new("Pippin","Chaos")
+p hobbit2
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,11 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+```Felt good to finally take on a challenge that I couldn't easily finish without any research or collaboration.  Felt very rewarding.```
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+``` Work flow is good I think.  I get up to use the restroom or eat when I need to and pace the room if I'm thinking.  I haven't felt distracted at any point.  I am in the Zone of Proximal Development.```
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+
+``` More collab, more study outside of class to stay fresh and learn new things.  Maintaining a healthy workout and sleep schedule at all costs!```
